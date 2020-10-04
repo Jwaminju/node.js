15dd2613ffbfab24a1b7d7cff2a0542244d4b5ac
wony0120 <49024958+Jwaminju@users.noreply.github.com>
2020-10-04 16:07:02 +0900

    ping -> pong
    1. get 방식으로 ping을 보내면, pong을 출력하기
    2. post 방식으로 ping을 보내고 pong 받기 (X)

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+/* GET home page. */
+router.get('/ping', function(req, res) {
+  res.send('pong')
+})
+
+//바디파서
+
+//router.post('/', function (req, res, next) {
+//    const body = req.body
+ //   console.log(body)
+  //  res.send('Get a random book')
+//})
+
+
+//put 
+
+//module.exports = router;
