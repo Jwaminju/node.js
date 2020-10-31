475bf59ddb4656f1fca0e60fc61e800eb6c26b64
wony0120 <49024958+Jwaminju@users.noreply.github.com>
2020-11-01 03:59:09 +0900

    Update index.js
    count
    
    
    
    
    request 객체의 method
    req.param : url에 포함된 변수
    req.query : url 바깥, ? 이후의 변수 <GET>
    req.body : XML, JSON, Multi Form 등의 데이터

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const router = express.Router()
 
-router.post('/', function(req, res){
-  const body = req.body
-  console.log(body.query)
-  res.send(`pong`)
+var count = 0;
+router.post('/', (req, res) => {
+  const {data} = req.body
+
+  if (data === 'ping') {
+      count++;
+      res.status(200).send('pong' + '(' + count +')')
+      return
+  }
+  res.status(400).send(`Are you serious?, ${data}`)
+ 
 })
-
-//app.use('/', router);
 module.exports = router
-
-/*app.get('/', function (req, res) {
-  res.send('root');
-});
-
-app.get('/about', function (req, res) {
-  res.send('about');
-});
-
-app.get('/random.text', function (req, res) {
-  res.send('random.text');
-});*/
 
